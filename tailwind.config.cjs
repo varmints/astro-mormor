18b8159d1931c7b5bf3c20f2ea4a6d16efefb9fc
Kamil <example@example.com>
2022-11-12 15:51:33 +0100

    color changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,18 @@ module.exports = {
         sans: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
       },
     },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      gray: {
+        100: "#f6f6f6",
+        200: "#e2e2e2",
+        300: "#8b8b8b",
+        400: "#6f6f6f",
+        500: "#3e3e3e",
+        600: "#222222",
+      },
+    },
   },
   plugins: [],
 };
