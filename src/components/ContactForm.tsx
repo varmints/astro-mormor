4dc9c139e659448edb51184e31312067be460176
Kamil <example@example.com>
2023-04-02 13:49:18 +0200

    some update

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -125,11 +125,11 @@ const ContactForm = () => {
             </label>
           </div>
           <div className="flex justify-center">
-            <Turnstile
+            {/* <Turnstile
               sitekey={captchaKey}
               onVerify={updateCaptchaToken}
               theme="dark"
-            />
+            /> */}
             <button
               disabled={submitting}
               className="flex items-center justify-center gap-3 border-2 border-current px-6 py-4 text-center hover:text-secondary focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2 focus:ring-offset-transparent"
