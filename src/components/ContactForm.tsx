65f14b3034cc71677d030714053abc53a67d3a8c
Kamil <example@example.com>
2023-05-21 16:57:43 +0200

    fix input focus

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -128,13 +128,13 @@ const ContactForm = () => {
               type="email"
               value={formState?.email}
               onChange={updateFormControl}
-              className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600"
+              className="peer h-10 w-full border-b-2 border-current bg-transparent placeholder-transparent text-sm focus:outline-none"
               placeholder="Adres email"
               required
             />
             <label
               htmlFor="email"
-              className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+              className="absolute -top-5 left-0 cursor-text text-offset transition-all text-sm peer-placeholder-shown:top-0 peer-placeholder-shown:text-current peer-placeholder-shown:text-base peer-focus:-top-5 peer-focus:text-offset peer-focus:text-sm"
             >
               Adres email
             </label>
